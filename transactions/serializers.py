18a0f2e7d1d64841f7b33b2d8f8f2389c03123b6
Moazin Khatri <moazinkhatri@gmail.com>
2018-11-29 23:12:07 +0500

    transactions done properly

---
+++ transactions/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from transactions.models import Transaction
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'email')
+
+class TransactionReadSerializer(serializers.ModelSerializer):
+    from_user = UserSerializer(read_only=True)
+    to_user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+class TransactionCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+        read_only_fields = ('date',)
--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from transactions.serializers import TransactionReadSerializer, TransactionCreateSerializer
+from transactions.models import Transaction
+
+class TransactionListCreateView(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionReadSerializer
+    serializer_class_write = TransactionCreateSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            if hasattr(self, 'serializer_class_write'):
+                return self.serializer_class_write
+        else:
+            return super().get_serializer_class()
