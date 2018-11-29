38f8f70b311438f89c788b736817d4356a4c5fc4
Moazin Khatri <moazinkhatri@gmail.com>
2018-11-29 18:20:29 +0500

    setup basic models

---
+++ transactions/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Transaction(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    amount = models.FloatField()
+    date = models.DateField(auto_now_add=True)
