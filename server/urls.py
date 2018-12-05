40b89eca273ba00aa1bd9716e8fe50c4e61994a8
Moazin Khatri <moazinkhatri@gmail.com>
2018-12-05 22:16:48 +0500

    custom auth obtain view done

--- server/urls.py
+++ server/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework.authtoken import views
+from users.views import CustomAuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', views.obtain_auth_token),
+    path('api-token-auth/', CustomAuthToken.as_view()),
     path('transactions/', include('transactions.urls')),
     path('users/', include('users.urls'))
 ]
--- users/views.py
+++ users/views.py
@@ -5,6 +5,8 @@ from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 from . import serializers
 
@@ -17,3 +19,20 @@ class UserCreateView(CreateAPIView):
 def UserProfileView(request):
     user_obj = serializers.UserSerializer(request.user)
     return Response(user_obj.data)
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'id': user.pk,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'username': user.username
+        })
